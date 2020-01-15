ccc0537abf9c9ff720d59a4c894bb416cfbf13fe
gauransh dingwani <gauransh7dingwani@gmail.com>
2020-01-15 17:52:22 +0530

    deleted unwanted files

--- admin/include/footer.php
+++
@@ -1,5 +0,0 @@
- <div class="footer">
-			<div class="wthree-copyright">
-                            <p>All rights reserved | Design by <a href=".../personal/index.html">Gauransh Dingwani</a></p>
-			</div>
-		  </div>
